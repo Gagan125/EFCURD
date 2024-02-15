f17b8f4ca8d24f9a8221647981b044aaf54c8f30
ASUS <ASUS@MSI>
2024-02-15 15:06:56 +0530

    Add project files.

---
+++ Models/appel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace loanApp.Models
+{
+    public class appel
+    {
+        public string name { get; set; }
+        public string password { get; set; }
+
+    }
+}
---
+++ Models/appelController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using loanApp.Models;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+
+namespace loanApp.Models
+{
+    public class appelController : Controller
+    {
+        // GET: appel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
